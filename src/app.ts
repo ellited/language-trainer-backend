068858f7b8767b67bdb3a925ba650c9cde1783be
Mike <rudenko.michael@gmail.com>
2024-01-16 21:25:37 +0100

    Add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express, {json, urlencoded} from "express";
 import { RegisterRoutes } from "../build/routes";
+import swaggerUi from "swagger-ui-express";
+import express, { json, urlencoded, Request, Response , NextFunction } from "express";
+
 
 export const app = express();
 
@@ -9,5 +11,14 @@ app.use(
     })
 );
 app.use(json());
+
+app.use("/docs", swaggerUi.serve, async (_req: Request, res: Response) => {
+    return res.send(swaggerUi.generateHTML(await import("../build/swagger.json")));
+});
+
+app.get("/swagger/swagger.json", async (req, res) => {
+    const swaggerJson = await import("../build/swagger.json");
+    res.send(swaggerJson);
+});
 
 RegisterRoutes(app);
